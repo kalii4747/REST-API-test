96c6db8acc2dbf3d8b8a357c60d76c8e5d26ef25
kalii4747 <pkalisz47@gmail.com>
2019-12-20 22:22:59 +0100

    Added PostgreSQL and psycopg2

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 17 18:21:42 2019
-
-@author: KALI CORP
-"""
-
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -17,11 +11,10 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'science_bitch'
 api = Api(app)
-
 
 jwt = JWT(app, authenticate, identity) #/auth
 
